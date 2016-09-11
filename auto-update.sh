23cfec901a95adf5a0c2318a0b2278bc254efa1a
Lectrode <electrodexsnet@gmail.com>
2016-09-11 16:17:56 -0600

    [FEATURE] v1.2 Support $HOME not in /home
     * [getsessions()] now retrieves users' $HOME
     * Notifications no longer require users' home in /home
    
    Signed-off-by: Lectrode <electrodexsnet@gmail.com>

--- auto-update.sh
+++ auto-update.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-#Auto Update v1.1 2016-08-28 For Arch Linux (Manjaro Xfce) by Lectrode
+#Auto Update v1.2 2016-09-11 For Manjaro Xfce by Lectrode
 #-Downloads and Installs new updates
 #-Depends: apacman, xfce4-notifyd, cut, grep, ping, su
-#This assumes all users have profile folders stored in /home
 
 debgn=+x; #-x =debugging, +x =no debugging
 set $debgn
 
-logdir="/var/log/XS"; logfile=$logdir/auto-update.log; mkdir -p "$logdir";
+logdir="/var/log/XS";
+logfile=$logdir/auto-update.log;
 
 pacclean(){ 
 paccache -rvk0;
@@ -26,23 +26,24 @@ finalmsg_normal(){ killmsg; iconnormal; sendall "$msg"; sleep 20; killmsg; }
 finalmsg_critical(){ killmsg; iconcritical; sendall "Kernel and/or drivers were updated. A restart is highly advised"; mv -f "$logfile" "${logfile}_`date -I`"; logfile=${logfile}_`date -I`; }
 
 getsessions(){ DEFAULTIFS=$IFS; CUSTOMIFS=$(echo -en "\n\b"); IFS=$CUSTOMIFS;
-i=0; while [ $i -lt ${#s_usr[@]} ]; do unset s_usr[$i]; unset s_disp[$i]; i=$(($i+1)); done;
+i=0; while [ $i -lt ${#s_usr[@]} ]; do unset s_usr[$i]; unset s_disp[$i]; unset s_home[$i]; i=$(($i+1)); done;
 i=0; for sssn in `loginctl list-sessions --no-legend`; do IFS=' '; sssnarr=($sssn);
 actv=$(loginctl show-session -p Active ${sssnarr[0]}|cut -d '=' -f 2);
 [[ "$actv" = "yes" ]] || continue;
 usr=$(loginctl show-session -p Name ${sssnarr[0]}|cut -d '=' -f 2);
 disp=$(loginctl show-session -p Display ${sssnarr[0]}|cut -d '=' -f 2);
-[[  ${usr-x} && ${disp-x} ]] || continue;
-s_usr[$i]=$usr; s_disp[$i]=$disp; i=$(($i+1)); IFS=$CUSTOMIFS; done;
-IFS=$DEFAULTIFS; unset i; unset usr; unset disp; unset actv; unset sssnarr; unset sssn; }
+usrhome=$(getent passwd "$usr"|cut -d: -f6); #alt: eval echo "~$usr"
+[[  ${usr-x} && ${disp-x} && ${usrhome-x} ]] || continue;
+s_usr[$i]=$usr; s_disp[$i]=$disp; s_home[$i]=$usrhome; i=$(($i+1)); IFS=$CUSTOMIFS; done;
+IFS=$DEFAULTIFS; unset i; unset usr; unset disp; unset usrhome; unset actv; unset sssnarr; unset sssn; }
 
 backgroundnotify(){ while : ; do getsessions;
-i=0; while [ $i -lt ${#s_usr[@]} ]; do if [ -f "/home/${s_usr[$i]}/.cache/XS/logonnotify" ]; then
+i=0; while [ $i -lt ${#s_usr[@]} ]; do if [ -f "${s_home[$i]}/.cache/XS/logonnotify" ]; then
 iconwarn; sendmsg "${s_usr[$i]}" "${s_disp[$i]}" "System is updating (please do not turn off the computer)\nDetails: $logfile"; 
-rm -f "/home/${s_usr[$i]}/.cache/XS/logonnotify"; fi; i=$(($i+1)); sleep 2; done; done; };
+rm -f "${s_home[$i]}/.cache/XS/logonnotify"; fi; i=$(($i+1)); sleep 2; done; done; };
 
-userlogon(){ if [ ! -d "/home/$USER/.cache/XS" ]; then mkdir -p "/home/$USER/.cache/XS"; fi;
-echo "This is a temporary file. It will be removed automatically" > /home/$USER/.cache/XS/logonnotify; 
+userlogon(){ if [ ! -d "$HOME/.cache/XS" ]; then mkdir -p "$HOME/.cache/XS"; fi;
+echo "This is a temporary file. It will be removed automatically" > "$HOME/.cache/XS/logonnotify"; 
 if [ ! -f "$logfile" ]; then if [ -f "${logfile}_lastkernel" ]; then
 iconcritical; notify-send -i $icon XS-AutoUpdate -u critical "Kernel and/or drivers were updated. A restart is highly advised"; fi; fi; }
 
@@ -55,7 +56,7 @@ if [ "$1" = "backnotify" ]; then backgroundnotify; exit 0; fi;
 if [ "$1" = "userlogon" ]; then userlogon; exit 0; fi;
 
 #Network and misc checks
-if [ ! -f "$logfile" ]; then echo "init">$logfile; fi;
+mkdir -p "$logdir"; if [ ! -f "$logfile" ]; then echo "init">$logfile; fi;
 if pidof -o %PPID -x "`basename "$0"`">/dev/null; then exit 0; fi; #Only 1 main instance allowed
 if [ $# -eq 0 ]; then "$0" "XS"& exit 0; fi;                       #Run in background
 checkwifi; waiting=1;waited=0; while [ $waiting = 1 ]
@@ -66,9 +67,9 @@ sleep 8; pgrep apacman && exit;
 
 #Start log and background notifications
 echo "init">$logfile; getsessions; i=0; while [ $i -lt ${#s_usr[@]} ]; do
-if [ -d "/home/${s_usr[$i]}/.cache" ]; then
-mkdir -p "/home/${s_usr[$i]}/.cache/XS"; echo "tmp" > /home/${s_usr[$i]}/.cache/XS/logonnotify;
-chown -R ${s_usr[$i]} "/home/${s_usr[$i]}/.cache/XS"; fi; i=$(($i+1)); done;
+if [ -d "${s_home[$i]}/.cache" ]; then
+mkdir -p "${s_home[$i]}/.cache/XS"; echo "tmp" > "${s_home[$i]}/.cache/XS/logonnotify";
+chown -R ${s_usr[$i]} "${s_home[$i]}/.cache/XS"; fi; i=$(($i+1)); done;
 "$0" "backnotify"& bkntfypid=$!;
 
 #Check for, download, and install updates; Remove obsolete packages
