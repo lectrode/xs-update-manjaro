4a275c0462985f3f9fe2202950f307fca9ef7032
Lectrode <electrodexsnet@gmail.com>
2016-09-11 13:37:38 -0600

    [BUGFIX] v0.3 Fix bug caused by dependency change
    A recent change in the output of [who] broke parsing to retrieve
    $DISPLAY for various logged-on users. To prevent this from happening in
    the future, [who] is no longer parsed. Instead, [loginctl] is used to
    get session information.
    
    Signed-off-by: Lectrode <electrodexsnet@gmail.com>

--- auto-update.sh
+++ auto-update.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-#Auto Update v0.2 2016-03-11 For Arch Linux (Manjaro Xfce) by Lectrode
+#Auto Update v0.3 2016-04-08 For Arch Linux (Manjaro Xfce) by Lectrode
 #-Downloads and Installs new updates
 #-Depends: apacman, xfce4-notifyd, awk, grep, ping, su
 #This assumes all users have profile folders stored in /home
@@ -11,13 +11,17 @@ pacclean="pacman -Scc --noconfirm";
 #Notification Functions
 killmsg(){ killall xfce4-notifyd; }
 getdisp(){ disp=$(who | awk -v user="$usr" '$1 == user && $2 ~ "^:" {print $2}'); };
-sendmsg(){ getdisp; DISPLAY=$disp su $usr -c "notify-send -i ElectrodeXS XS-AutoUpdate -u critical \"$1\""; };
+sendmsg(){ DISPLAY=$disp su $usr -c "notify-send -i ElectrodeXS XS-AutoUpdate -u critical \"$1\""; };
 sendall(){ for usr in `ls /home|grep -v "Public"`; do sendmsg "$1"; done; }
 
-backgroundnotify(){ while : ; do for usr in `ls /home|grep -v "Public"`;
-do if [ -f "/home/$usr/.cache/XS/logonnotify" ]; then
+backgroundnotify(){ while : ; do DEFAULTIFS=$IFS; CUSTOMIFS=$(echo -en "\n\b"); IFS=$CUSTOMIFS;
+for sssn in `loginctl list-sessions --no-legend`
+do IFS=' '; unset usr; unset disp; unset sssnarr; sssnarr=($sssn);
+usr=$(loginctl show-session -p Name ${sssnarr[0]}|cut -d '=' -f 2);
+disp=$(loginctl show-session -p Display ${sssnarr[0]}|cut -d '=' -f 2);
+[[  ${usr-x} && ${disp-x} ]] && if [ -f "/home/$usr/.cache/XS/logonnotify" ]; then
 sendmsg "System is updating (please do not turn off the computer)\nDetails: $logfile"; 
-rm -f "/home/$usr/.cache/XS/logonnotify"; fi; sleep 2; done; done; };
+rm -f "/home/$usr/.cache/XS/logonnotify"; fi; sleep 2; IFS=$CUSTOMIFS; done; done; IFS=$DEFAULTIFS; };
 
 userlogon(){ if [ ! -d "/home/$USER/.cache/XS" ]; then mkdir -p "/home/$USER/.cache/XS"; fi;
 echo "This is a temporary file. It will be removed automatically" > /home/$USER/.cache/XS/logonnotify; }
@@ -44,10 +48,11 @@ mkdir -p "/home/$usr/.cache/XS"; echo "tmp" > /home/$usr/.cache/XS/logonnotify; 
 ($pacclean; pacman-mirrors -g -c United_States;)  2>&1 |tee -a $logfile;
 sudo pacman -S --needed --noconfirm archlinux-keyring manjaro-keyring manjaro-system 2>&1 |tee -a $logfile;
 (pacman-key --refresh-keys; pacman-optimize; sync;)  2>&1 |tee -a $logfile;
-apacman -Syyu --needed --noconfirm  2>&1 |tee -a $logfile
+apacman -Syyu --needed --noconfirm 2>&1 |tee -a $logfile
 (pacman -Rnsc $(pacman -Qtdq) --noconfirm; $pacclean;)  2>&1 |tee -a $logfile
 
 #Finish
+sleep 5;
 kill $bkntfypid;
 msg="System update finished"; grep "Total Installed Size:" $logfile && msg="$msg \nPackages successfully updated"
 grep "new signatures:" $logfile && msg="$msg \nSecurity signatures updated"
@@ -55,5 +60,5 @@ grep "Total Removed Size:" $logfile && msg="$msg \nObsolete packages removed"
 if [ ! "$msg" = "System update finished" ]; then msg="$msg \nDetails: $logfile"; fi;
 if [ "$msg" = "System update finished" ]; then msg="System up-to-date, no changes made"; fi;
 killmsg; sendall "$msg"; sleep 10; killmsg;
-grep -v "warning" $logfile |grep -E "linux[0-9]{2,3}" && (killmsg; sendall "Kernel and/or drivers were updated. A restart is highly advised";)
+grep -v "warning" $logfile |grep -E "linux[0-9]{2,3}" && (killmsg; sendall "Kernel and/or drivers were updated. A restart is highly advised"; mv -f "$logfile" "${logfile}_lastkernel"; logfile=${logfile}_lastkernel;)
 echo "XS-done">>$logfile; exit 0;
