3da27cc72f22ed6a340be743a0765c9eb4a26048
Lectrode <electrodexsnet@gmail.com>
2016-09-11 14:05:37 -0600

    v1.0 First official release
    [BUGFIX]
     * Always get user sessions for status notifications
     * Additional filtering to prevent false detections of critical updates
     * Do not send finish notification before critical notification
    
    [FEATURE]
     * Send notification after critical updates to user at login
     * Custom Icons for Update progress and completion
     * Save log for last critical updates
     * Add debugging switch
    
    [CONF]
     * Downgrade packages if installed is later than source repository
    
    [TASK]
     * Remove [awk] dependency
     * Reduce redundancy with reusable functions
     * Simplify code
    
    Signed-off-by: Lectrode <electrodexsnet@gmail.com>

--- auto-update.sh
+++ auto-update.sh
@@ -1,36 +1,57 @@
-#!/bin/sh
-#Auto Update v0.3 2016-04-08 For Arch Linux (Manjaro Xfce) by Lectrode
+#!/bin/bash
+#Auto Update v1.0 2016-04-26 For Arch Linux (Manjaro Xfce) by Lectrode
 #-Downloads and Installs new updates
-#-Depends: apacman, xfce4-notifyd, awk, grep, ping, su
+#-Depends: apacman, xfce4-notifyd, cut, grep, ping, su
 #This assumes all users have profile folders stored in /home
 
+debgn=+x; #-x =debugging, +x =no debugging
+set $debgn
+
 logdir="/var/log/XS"; logfile=$logdir/auto-update.log; mkdir -p "$logdir";
-pacclean="pacman -Scc --noconfirm";
-#pacclean="paccache -rvk3";
+
+pacclean(){ 
+paccache -rvk0;
+rm -rf /var/cache/apacman/pkg/*;
+#paccache -rvk3;
+}
 
 #Notification Functions
 killmsg(){ killall xfce4-notifyd; }
-getdisp(){ disp=$(who | awk -v user="$usr" '$1 == user && $2 ~ "^:" {print $2}'); };
-sendmsg(){ DISPLAY=$disp su $usr -c "notify-send -i ElectrodeXS XS-AutoUpdate -u critical \"$1\""; };
-sendall(){ for usr in `ls /home|grep -v "Public"`; do sendmsg "$1"; done; }
+iconnormal(){ icon=ElectrodeXS; }
+iconwarn(){ icon=software-update-urgent-symbolic; }
+iconcritical(){ icon=software-update-urgent; }
+sendmsg(){ DISPLAY=$2 su $1 -c "notify-send -i $icon XS-AutoUpdate -u critical \"$3\""; }
+sendall(){ getsessions; i=0; while [ $i -lt ${#s_usr[@]} ]; do sendmsg "${s_usr[$i]}" "${s_disp[$i]}" "$1"; i=$(($i+1)); done; unset i; }
+finalmsg_normal(){ killmsg; iconnormal; sendall "$msg"; sleep 20; killmsg; }
+finalmsg_critical(){ killmsg; iconcritical; sendall "Kernel and/or drivers were updated. A restart is highly advised"; mv -f "$logfile" "${logfile}_`date -I`"; logfile=${logfile}_`date -I`; }
 
-backgroundnotify(){ while : ; do DEFAULTIFS=$IFS; CUSTOMIFS=$(echo -en "\n\b"); IFS=$CUSTOMIFS;
-for sssn in `loginctl list-sessions --no-legend`
-do IFS=' '; unset usr; unset disp; unset sssnarr; sssnarr=($sssn);
+getsessions(){ DEFAULTIFS=$IFS; CUSTOMIFS=$(echo -en "\n\b"); IFS=$CUSTOMIFS;
+i=0; while [ $i -lt ${#s_usr[@]} ]; do unset s_usr[$i]; unset s_disp[$i]; i=$(($i+1)); done;
+i=0; for sssn in `loginctl list-sessions --no-legend`; do IFS=' '; sssnarr=($sssn);
+actv=$(loginctl show-session -p Active ${sssnarr[0]}|cut -d '=' -f 2);
+[[ "$actv" = "yes" ]] || continue;
 usr=$(loginctl show-session -p Name ${sssnarr[0]}|cut -d '=' -f 2);
 disp=$(loginctl show-session -p Display ${sssnarr[0]}|cut -d '=' -f 2);
-[[  ${usr-x} && ${disp-x} ]] && if [ -f "/home/$usr/.cache/XS/logonnotify" ]; then
-sendmsg "System is updating (please do not turn off the computer)\nDetails: $logfile"; 
-rm -f "/home/$usr/.cache/XS/logonnotify"; fi; sleep 2; IFS=$CUSTOMIFS; done; done; IFS=$DEFAULTIFS; };
+[[  ${usr-x} && ${disp-x} ]] || continue;
+s_usr[$i]=$usr; s_disp[$i]=$disp; i=$(($i+1)); IFS=$CUSTOMIFS; done;
+IFS=$DEFAULTIFS; unset i; unset usr; unset disp; unset actv; unset sssnarr; unset sssn; }
+
+backgroundnotify(){ while : ; do getsessions;
+i=0; while [ $i -lt ${#s_usr[@]} ]; do if [ -f "/home/${s_usr[$i]}/.cache/XS/logonnotify" ]; then
+iconwarn; sendmsg "${s_usr[$i]}" "${s_disp[$i]}" "System is updating (please do not turn off the computer)\nDetails: $logfile"; 
+rm -f "/home/${s_usr[$i]}/.cache/XS/logonnotify"; fi; i=$(($i+1)); sleep 2; done; done; };
 
 userlogon(){ if [ ! -d "/home/$USER/.cache/XS" ]; then mkdir -p "/home/$USER/.cache/XS"; fi;
-echo "This is a temporary file. It will be removed automatically" > /home/$USER/.cache/XS/logonnotify; }
+echo "This is a temporary file. It will be removed automatically" > /home/$USER/.cache/XS/logonnotify; 
+if [ ! -f "$logfile" ]; then if [ -f "${logfile}_lastkernel" ]; then
+iconcritical; notify-send -i $icon XS-AutoUpdate -u critical "Kernel and/or drivers were updated. A restart is highly advised"; fi; fi; }
 
 #Start Sub-processes
 if [ "$1" = "backnotify" ]; then backgroundnotify; exit 0; fi;
 if [ "$1" = "userlogon" ]; then userlogon; exit 0; fi;
 
 #Network and misc checks
+if [ ! -f "$logfile" ]; then echo "init">$logfile; fi;
 if pidof -o %PPID -x "`basename "$0"`">/dev/null; then exit 0; fi; #Only 1 main instance allowed
 if [ $# -eq 0 ]; then "$0" "XS"& exit 0; fi;                       #Run in background
 waiting=1;waited=0; while [ $waiting = 1 ]
@@ -45,20 +66,20 @@ mkdir -p "/home/$usr/.cache/XS"; echo "tmp" > /home/$usr/.cache/XS/logonnotify; 
 "$0" "backnotify"& bkntfypid=$!;
 
 #Check for, download, and install updates; Remove obsolete packages
-($pacclean; pacman-mirrors -g -c United_States;)  2>&1 |tee -a $logfile;
+(pacclean; pacman-mirrors -g -c United_States;)  2>&1 |tee -a $logfile;
 sudo pacman -S --needed --noconfirm archlinux-keyring manjaro-keyring manjaro-system 2>&1 |tee -a $logfile;
 (pacman-key --refresh-keys; pacman-optimize; sync;)  2>&1 |tee -a $logfile;
-apacman -Syyu --needed --noconfirm 2>&1 |tee -a $logfile
-(pacman -Rnsc $(pacman -Qtdq) --noconfirm; $pacclean;)  2>&1 |tee -a $logfile
+apacman -Syyuu --needed --noconfirm 2>&1 |tee -a $logfile
+(pacman -Rnsc $(pacman -Qtdq) --noconfirm; pacclean;)  2>&1 |tee -a $logfile
+#sleep 5;
 
 #Finish
-sleep 5;
 kill $bkntfypid;
 msg="System update finished"; grep "Total Installed Size:" $logfile && msg="$msg \nPackages successfully updated"
 grep "new signatures:" $logfile && msg="$msg \nSecurity signatures updated"
 grep "Total Removed Size:" $logfile && msg="$msg \nObsolete packages removed"
 if [ ! "$msg" = "System update finished" ]; then msg="$msg \nDetails: $logfile"; fi;
 if [ "$msg" = "System update finished" ]; then msg="System up-to-date, no changes made"; fi;
-killmsg; sendall "$msg"; sleep 10; killmsg;
-grep -v "warning" $logfile |grep -E "linux[0-9]{2,3}" && (killmsg; sendall "Kernel and/or drivers were updated. A restart is highly advised"; mv -f "$logfile" "${logfile}_lastkernel"; logfile=${logfile}_lastkernel;)
+normcrit=norm; grep -v "warning" $logfile |grep -v "removed"|grep -v "tor-browser"|grep -E "linux[0-9]{2,3}" && normcrit=crit
+[[ "$normcrit" = "norm" ]] && finalmsg_normal; [[ "$normcrit" = "crit" ]] && finalmsg_critical;
 echo "XS-done">>$logfile; exit 0;
